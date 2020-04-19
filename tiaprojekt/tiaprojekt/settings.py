fe8cb43389fe7b2bfdb6ce343f3c2be77c1e0697
Szabova Dominika <szabova.dominika@gmail.com>
2020-04-20 01:45:27 +0200

    changing root for heroku 4

--- tiaprojekt/tiaprojekt/settings.py
+++ tiaprojekt/tiaprojekt/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tiaprojekt.tiaprojekt.apps.KvizConfig',
+    'tiaprojekt.apps.KvizConfig',
 ]
 
 MIDDLEWARE = [
